e816c4dcbe389d1c33e420f09266170e56229ae2
Bobandraa <sandra.cohen.ux@gmail.com>
2024-10-06 17:06:58 -0700

    feat(frontend): add frontend app routing navbar

---
+++ MERN-app/frontend/src/components/Navbar.jsx
@@ -0,0 +1,44 @@
+import { Button, Container, Flex, HStack, Link, Text, useColorMode } from '@chakra-ui/react'
+import React from 'react'
+
+import { PlusSquareIcon } from "@chakra-ui/icons";
+import { IoMoon } from "react-icons/io5";
+import { LuSun } from "react-icons/lu";
+
+const Navbar = () => {
+    const {colorMode, toggleColorMode} = useColorMode();
+
+    return (
+        <Container maxW={"1140px"} px={4}> 
+            <Flex
+            h={16}
+            alignItems={"center"}
+            justifyContent={"space-between"}
+            flexDir={{base: "column", sm: "row"}}
+            >
+                <Text
+                fontSize={{base: "22", sm: "28"}}
+                fontWeight={"bold"}
+                textTransform={"upperCase"}
+                textAlign={"center"}
+                bgGradient={"linear(to-r, #7928CA, #FF0080)"}
+                bgClip={"text"}
+                >
+                    <Link to="/">🛒 Product Store</Link>
+                </Text>
+                <HStack spacing={2} alignItems={"center"}>
+                    <Link to={"/create"}>
+                        <Button>
+                            <PlusSquareIcon fontSize={22} />
+                        </Button>
+                    </Link>
+                    <Button onClick={toggleColorMode}> 
+                        {colorMode === "light" ? <IoMoon /> : <LuSun size={"20"}/>} 
+                    </Button>
+                </HStack>
+            </Flex>
+        </Container>
+    )
+}
+
+export default Navbar
