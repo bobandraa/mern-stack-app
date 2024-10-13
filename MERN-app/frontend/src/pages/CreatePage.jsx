88bb0f0714b519479aedd7a1bff8ef7e00faae00
Bobandraa <sandra.cohen.ux@gmail.com>
2024-10-13 16:30:19 -0700

    feat(frontend): adds input and button fields to /create

--- MERN-app/frontend/src/pages/CreatePage.jsx
+++ MERN-app/frontend/src/pages/CreatePage.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+import { Box, Button, Container, Heading, useColorModeValue, VStack, Input } from '@chakra-ui/react'
+import React, { useState } from 'react'
 
 const CreatePage = () => {
+  const [newProduct, setNewProduct] = useState({
+    name: "",
+    price: "",
+    image: ""
+  })
+
+  const handleAddProduct = () => {
+    console.log(newProduct);
+  }
+
   return (
-    <div>CreatePage</div>
+    <Container maxW={"container.sm"}>
+      <VStack spacing={8}>
+
+        <Heading as="h1" size={"2xl"} textAlign={"center"} mb={8}>
+          Create New Product
+        </Heading>
+
+        <Box w={"full"} bg={useColorModeValue("white", "gray.800")}
+          p={6} rounded={"lg"} shadow={"md"}
+        >
+          <VStack>
+            <Input 
+              placeholder='New Product'
+              name='name'
+              value={newProduct.name}
+              onChange={(e) => setNewProduct({...newProduct, name: e.target.value})} 
+            />
+            <Input 
+              placeholder='New Price'
+              name='price'
+              type='number'
+              value={newProduct.price}
+              onChange={(e) => setNewProduct({...newProduct, price: e.target.value})} 
+            />
+            <Input 
+              placeholder='New Image'
+              name='image'
+              value={newProduct.image}
+              onChange={(e) => setNewProduct({...newProduct, image: e.target.value})} 
+            />
+          </VStack>
+
+          <Button colorScheme='blue' onClick={handleAddProduct} w='full'>
+            Add Product
+          </Button>
+
+        </Box>
+
+      </VStack>
+
+    </Container>
   )
 }
 
